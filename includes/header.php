4dfdc0901e23cf3932b6286b77345c836f529829
mooizahmed102 <amoiz6263@gmail.com>
2023-10-23 21:02:44 +0500

    first commit

---
+++ includes/header.php
@@ -0,0 +1,87 @@
+<header class="cs-site_header cs-style1 text-uppercase cs-sticky_header">
+      <div class="cs-main_header">
+        <div class="container">
+          <div class="cs-main_header_in">
+            <div class="cs-main_header_left">
+              <a class="cs-site_branding" href="/">
+                <img src="assets/img/Teals-Technologeis.png" alt="Logo">
+              </a>
+            </div>
+            <div class="cs-main_header_center">
+              <div class="cs-nav cs-primary_font cs-medium">
+                <ul class="cs-nav_list">
+                  <li class="">
+                    <a href="home/">Home</a>
+                    <!--  -->
+                  </li>
+                  <li>
+                    <a href="about/">About</a>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="service/">Services</a>
+                    <ul>
+                      <li>
+                        <a href="web-development/">Web development</a>
+                      </li>
+                      <li>
+                        <a href="mobile-app/">Mobile App</a>
+                      </li>
+                      <li>
+                        <a href="smm-marketing/">SMM marketing</a>
+                      </li>
+                      <li>
+                        <a href="digital/">digital marketing</a>
+                      </li>
+                      <li>
+                        <a href="content-writing/">content writing</a>
+                      </li>
+                      <li>
+                        <a href="logo-design/">logo design</a>
+                      </li>
+                      <li>
+                        <a href="2d-3d-design/">2d &3d design</a>
+                      </li>
+                      <li>
+                        <a href="video-animation//">video animation</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="">
+                    <a href="portfolio/">Portfolio</a>
+                    <!-- <ul>
+                      <li>
+                        <a href="portfolio/">Portfolio</a>
+                      </li>
+                      <li>
+                        <a href="portfolio-details/">Portfolio Details</a>
+                      </li>
+                    </ul> -->
+                  </li>
+                  <li class="">
+                    <a href="blog/">Blog</a>
+                    
+                  </li>
+                  
+                  <li class="">
+                  <a href="contact/">Contact</a>
+                    
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="cs-main_header_right">
+              <div class="cs-toolbox">
+                <span class="cs-icon_btn">
+                  <span class="cs-icon_btn_in">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
